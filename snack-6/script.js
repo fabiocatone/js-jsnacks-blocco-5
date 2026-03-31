dcc178f5bc03c22526f220b74733200ca2aa76ca
fabiocatone <fabbiocatone@email.com>
2026-03-31 16:02:34 +0200

    ok snack6

--- snack-6/script.js
+++ snack-6/script.js
@@ -11,4 +11,15 @@ const zucchine = [
   { type: 'Calabrese', weight: 14, length: 4 },
 ];
 
+const longzucchine = zucchine.filter((Element) => {
+  if (Element.length > 15 ) return true
+}
+)
+
+const shortzucchine = zucchine.filter((element)  => {
+  if (element.length < 15) return true
+})
+
+console.log(longzucchine,shortzucchine)
+
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
