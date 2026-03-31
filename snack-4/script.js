55084684bf1fd9e40e6de6cce1a451e2d449ed72
fabiocatone <fabbiocatone@email.com>
2026-03-31 15:50:06 +0200

    ok snack4

--- snack-4/script.js
+++ snack-4/script.js
@@ -5,5 +5,11 @@ const posts = [
   { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
 ]
 
+const authorposts = posts.map((element) => {
+  return element.author
+})
+
+console.log(authorposts)
+
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
