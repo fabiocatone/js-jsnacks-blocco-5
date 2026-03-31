d0fd9cbd40e1c72627aac4362df58b859dba88dc
fabiocatone <fabbiocatone@email.com>
2026-03-31 15:48:24 +0200

    ok snack3

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,4 +1,10 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
+const plus1numbers = numbers.map((element) => {
+    return element + 1
+}
+)
+console.log(plus1numbers)
+
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
