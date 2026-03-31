90abce9d29c63cd8890a450e868da1eceaf69907
fabiocatone <fabbiocatone@email.com>
2026-03-31 15:43:44 +0200

    ok snack1

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,10 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
 
+names.forEach((element) => {
+    console.log(element);
+    
+})
+
+
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
